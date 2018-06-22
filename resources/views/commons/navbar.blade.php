c83645806f2cfbd769384238bd8fb0fc75edb52c
kaho.kobayashi <kaho.kobayashi@rakuten.com>
2018-06-22 06:28:25 +0000

    finalmonolist

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -14,12 +14,12 @@
                 <ul class="nav navbar-nav navbar-right">
                     @if (Auth::check())
                         <li>
-                            <a href="#">
+                            <a href="{{ route('items.create') }}">
                                 <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
                                 アイテムを追加
-                              </a>
+                            </a>
                         </li>
-
+                        
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                                 <span class="glyphicon glyphicon-signal" aria-hidden="true"></span>
